206bd0fe98fab20be9d0819311afe9aa42323168
Nikita <Niktube2222@gmail.com>
2024-10-03 21:07:07 +0300

    added documentation

--- circle.py
+++ circle.py
@@ -2,9 +2,15 @@ import math
 
 
 def area(r):
+	'''
+	возвращает площадь круга
+	'''
     return math.pi * r * r
 
 
 def perimeter(r):
+	'''
+	возвращает периметр круга
+	'''
     return 2 * math.pi * r
 
