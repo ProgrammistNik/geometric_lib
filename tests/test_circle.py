5b97db2301c89c5ec95bb03c51b4923001f6b7b3
Nikita Sokolov <95882225+ProgrammistNik@users.noreply.github.com>
2024-11-19 18:28:33 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,8 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from circle import area, perimeter
+sys.path.insert\
+    (0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_area():
