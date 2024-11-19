08d0d9777dd0764badd5e9e5338774d1cc26f691
Nikita Sokolov <95882225+ProgrammistNik@users.noreply.github.com>
2024-11-19 18:42:36 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -2,8 +2,8 @@ import sys
 import os
 import pytest
 from calculate import calc
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 )
 
 
