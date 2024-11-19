62e12511f3817d9eb43d5df277d1a32e84f846cb
Nikita Sokolov <95882225+ProgrammistNik@users.noreply.github.com>
2024-11-19 18:11:19 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -7,7 +7,7 @@ from circle import area, perimeter
 
 def test_area():
     result = area(2)
-    expected_result = 12,566
+    expected_result = 12.566
     assert result == pytest.approx(expected_result, rel=5e-2)
 
 
@@ -18,7 +18,7 @@ def test_area_invalid():
 
 def test_perimeter():
     result = perimeter(2)
-    expected_result = 12,566
+    expected_result = 12.566
     assert result == pytest.approx(expected_result, rel=5e-2)
 
 
