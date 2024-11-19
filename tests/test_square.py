fd88ee67563cba261b7178e0eee3f5263f896670
Nikita Sokolov <95882225+ProgrammistNik@users.noreply.github.com>
2024-11-19 18:34:43 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,8 +1,10 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from square import area, perimeter
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
 
 
 def test_area():
