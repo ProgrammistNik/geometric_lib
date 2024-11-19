5ae4a53e9fc5383f7a12608f977e146306cf4e97
Nikita Sokolov <95882225+ProgrammistNik@users.noreply.github.com>
2024-11-19 18:06:25 +0300

    Update test_triangle.py

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,37 +1,35 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import unittest
+import pytest
 from triangle import area, perimeter
 
 
-def test_area(self):
-    self.assertEqual(area(5, 12, 13), 30)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-def test_area_invalid(self):
-    with self.assertRaises(TypeError):
+def test_area():
+    assert area(5, 12, 13) == 30
+
+
+def test_area_invalid():
+    with pytest.raises(TypeError):
         area(5, 12, "13")
 
 
-def test_invalid_size_area(self):
-    with self.assertRaises(TypeError):
+def test_invalid_size_area():
+    with pytest.raises(TypeError):
         area(5, 12)
 
 
-def test_perimeter(self):
-    self.assertEqual(perimeter(5, 12, 13), 30)
+def test_perimeter():
+    assert perimeter(5, 12, 13) == 30
 
 
-def test_perimeter_invalid(self):
-    with self.assertRaises(TypeError):
+def test_perimeter_invalid():
+    with pytest.raises(TypeError):
         perimeter(5, 12, "13")
-            
 
-def test_invalid_size_perimeter(self):
-    with self.assertRaises(TypeError):
+
+def test_invalid_size_perimeter():
+    with pytest.raises(TypeError):
         perimeter(5, 12)
-            
-
-if __name__ == '__main__':
-    unittest.main()
